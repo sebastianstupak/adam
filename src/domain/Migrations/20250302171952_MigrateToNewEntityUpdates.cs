6a7bc3fcd16ea72389446b038c0dc81abc29b002
Minkuh <michalkralovic2@gmail.com>
2025-03-02 18:23:28 +0100

    Add latest migrations

---
+++ src/domain/Migrations/20250302171952_MigrateToNewEntityUpdates.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ADAM.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateToNewEntityUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Subscriptions",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Subscriptions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
